90a72c9b8bce6c28da2e154994d5bcc12bc725ff
Erin Hill <erinicole1988@gmail.com>
2019-10-03 13:14:41 -0700

    the grass now moves across the screen

--- src/index.js
+++ src/index.js
@@ -5,48 +5,42 @@ window.Character = Character;
 
 window.addEventListener("DOMContentLoaded", event => {
   const canvas = document.getElementById("story-canvas");
-  canvas.style.width =
-    '100vw';
-   canvas.style.height =
-     '100vh';
+  canvas.style.width = "100vw";
+  canvas.style.height = "100vh";
   const ctx = canvas.getContext("2d");
   window.ctx = ctx;
-  const girl = new Character({x: 0, y: 100, ctx});
+  const girl = new Character({ x: 0, y: 100, ctx });
   girl.drawImage();
-        const width = document.documentElement.clientWidth
-        const height = document.documentElement.clientHeight
-        console.log(height)
-        console.log(width);
-//   const draw = () => {
-    // this paints the sky
-    ctx.fillStyle = `rgb(227, 254, 255)`;
-    ctx.fillRect(0,0, width, height);
-    //this paints the ground
-    ctx.beginPath();
-    ctx.rect(0, 140, width, 10);
-    ctx.fillStyle = `rgb(130, 79, 43)`;
-    ctx.fill();
-    // ctx.rect(0,0, width, )
-    // ctc.fillStyle = rgb(227, 254, 255);
-    // ctx.fill();
+  const width = document.documentElement.clientWidth;
+  const height = document.documentElement.clientHeight;
 
-    // ctx.fillStyle = rgb(130, 79, 43);
-    // ctx.rect(0, height*0.90, width, height*0.10);
-    
-    // draw the blocks
-    // for (var i = 0; i < grassXs.length; i++) {
-    //     const img = new Image();
-    //     img.src = '../images/grass.png';
-    //     img.onload = () => {
-    //     ctx.drawImage(img, this.x, this.y, 50, 50);
-    //     };
-    //     image(getImage("grass.jpg"), grassXs[i], height*0.85, 20, 20);
-    //     // subtract one, so that they appear to move to left (hopper appears to move to right)
-    //     grassXs[i] -= 1;
-    //     // Now move the blocks over once they wrap around
-    //     if (grassXs[i] <= -20) {
-    //         grassXs[i] = width;
-    //     }
-    // }
-    // draw()
+  // this paints the sky
+  ctx.fillStyle = `rgb(227, 254, 255)`;
+  ctx.fillRect(0, 0, width, height);
+  //this paints the ground
+  ctx.beginPath();
+  ctx.rect(0, 140, width, 10);
+  ctx.fillStyle = `rgb(130, 79, 43)`;
+  ctx.fill();
+  //this is the array that holds the grass image
+  const grassXs = [];
+  for (let i = 0; i < 4; i++) {
+    grassXs.push(i * 100);
+  }
+  console.log(grassXs);
+  const drawGrass = () => {
+    for (let i = 0; i < grassXs.length; i++) {
+      const img = new Image();
+      img.src = "../images/grass.png";
+      img.onload = () => {
+        ctx.drawImage(img, grassXs[i], 130, 100, 20);
+      };
+      grassXs[i] -= 1;
+      if (grassXs[i] <= -100) {
+        grassXs[i] = 300;
+      }
+    }
+  };
+//   const loopGrass = setInterval(() => drawGrass(), 150)
+//   loopGrass()
 });
