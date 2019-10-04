e0e767e685f68d25e0e652f5d0abf0fc85dcf21d
Erin Hill <erinicole1988@gmail.com>
2019-10-04 10:17:48 -0700

    pages are changed on left arrow and right arrow

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ window.addEventListener("DOMContentLoaded", event => {
   const body = document.getElementById("body");
   const canvas = document.getElementById("story-canvas");
   const textArea = document.getElementById("text-area");
-  textArea.innerHTML = allText()[0].text;
+  let page = 0
+  textArea.innerHTML = allText()[page].text;
   body.style.backgroundColor = `rgb(227, 254, 255)`;
   canvas.style.width = str;
   canvas.style.height = "100vh";
@@ -35,28 +36,52 @@ window.addEventListener("DOMContentLoaded", event => {
     grassXs.push(i * 100);
   }
   console.log(grassXs);
-  const drawGrass = () => {
-    girl.drawImage();
-    // ctx.fillStyle = `rgb(227, 254, 255)`;
-    // ctx.fillRect(0, 0, width, height);
-    // this paints the ground
-    // ctx.beginPath();
-    // ctx.rect(0, 140, width, 10);
-    // ctx.fillStyle = `rgb(130, 79, 43)`;
-    // ctx.fill();
-    for (let i = 0; i < grassXs.length; i++) {
-      const img = new Image();
-      img.src = "../images/grass.png";
-      img.onload = () => {
-        ctx.drawImage(img, grassXs[i], 130, 100, 20);
-      };
-      grassXs[i] -= 1;
-      if (grassXs[i] <= -100) {
-        grassXs[i] = 300;
-      }
-    }
-    ctx.clearRect(0, 0, width, height);
-  };
+   console.log("anyhting");
+   document.onkeydown = event => {
+     switch (event.keyCode) {
+       case 37:
+           if (page === 0){
+            textArea.innerHTML = allText()[0].text;
+           }
+           else if (page >= 0) {
+               page--
+             textArea.innerHTML = allText()[page].text;
+           }
+         break;
+       case 39:
+           if (page === 2){
+            textArea.innerHTML = allText()[2].text;
+           }
+           else if (page <= 2){
+               page++
+            textArea.innerHTML = allText()[page].text;}
+         break;
+     }
+   };
+//   const drawGrass = () => {
+//     girl.drawImage();
+//     // ctx.fillStyle = `rgb(227, 254, 255)`;
+//     // ctx.fillRect(0, 0, width, height);
+//     // this paints the ground
+//     // ctx.beginPath();
+//     // ctx.rect(0, 140, width, 10);
+//     // ctx.fillStyle = `rgb(130, 79, 43)`;
+//     // ctx.fill();
+//     for (let i = 0; i < grassXs.length; i++) {
+//       const img = new Image();
+//       img.src = "../images/grass.png";
+//       img.onload = () => {
+//         ctx.drawImage(img, grassXs[i], 130, 100, 20);
+//       };
+//       grassXs[i] -= 1;
+//       if (grassXs[i] <= -100) {
+//         grassXs[i] = 300;
+//       }
+//     }
+//     ctx.clearRect(0, 0, width, height);
+   
+
+//   };
   //   const loopGrass = () => setInterval(() => drawGrass(), 100)
   //   loopGrass()
 });
